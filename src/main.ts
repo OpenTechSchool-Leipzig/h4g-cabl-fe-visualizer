81bd3443a4054184435d6f795c4c9e83037fa1ee
Alex Muthmann <alex@elvah.de>
2022-10-08 17:25:58 +0200

    ChartJs Demo boilerplate

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import "./components/medium-articles";
+import "./components/medium-header";
+import { html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+const globalStyles = `
+<style>
+  @import url("https://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400,700");
+  @import url("https://fonts.googleapis.com/css?family=Raleway:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i");
+  :root {
+    --color-action: #e31b6d;
+    --color-bg: #d2dbdd;
+    --color-chip-bg: #f2f2f2;
+    --color-chip-bg-hover: #e6e6e6;
+  }
+
+  #medium-portfolio-app {
+    font-family: "Roboto Slab", serif;
+    background-color: var(--color-bg);
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    padding: 20px 0px;
+  }
+  </style>
+`;
